e9252d05dc27906ee76516b9be96a883acafa16f
Majharulislam <majhurulislam535@gmail.com>
2025-04-21 00:15:26 +0600

    create a chat application

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Join from './Components/Join.jsx'
+import Chat from './Components/Chat.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Join></Join>,
+  },
+  {
+    path:'/chat',
+    element:<Chat></Chat>
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
